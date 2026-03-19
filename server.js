6d49e69bd7177a4e880de011472393ed9810f763
Louis <drivebetter24@duck.com>
2026-03-19 16:14:16 +0300

    Add cars API routes

--- server.js
+++ server.js
@@ -3,8 +3,31 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-res.send("Welcome to Marine Hire System")
+app.use(express.json())
+
+// Temporary data storage
+const cars = [
+    { id: 1, make: 'Toyota', model: 'Camry', price: 2500 },
+    { id: 2, make: 'Honda', model: 'Civic', price: 22000 },
+    { id: 3, make: 'Ford', model: 'Mustang', price: 65000 }
+]
+
+
+app.get('/cars', (req, res) => {
+res.json(cars)
+})
+
+app.post('/cars', (req, res) => {
+    const newCar = req.body
+    cars.push(newCar)
+res.json(cars)
+})
+
+app.delete('/cars/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const index = cars.findIndex(car => car.id === id)  
+    cars.splice(index, 1)
+    res.json({message: `Car ${id} deleted successfully.`, cars})
 })
 
 app.listen(port, () =>{
